8a4ec8387cbebcbe9a482f7fd76108f953a01d8a
mindu23 <mindj5541@google.com>
2022-05-29 22:23:49 +0900

    테스트 코드 작성

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// json을 반환하는 컨트롤러
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
